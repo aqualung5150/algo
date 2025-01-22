5dd5044bf06dde871cbe907981654592018cddfc
Seungjoon Choi <tmdwns0845@naver.com>
2025-01-22 21:35:24 +0900

    feat: CORS 설정

--- src/main/java/com/seungjoon/algo/config/SecurityConfig.java
+++ src/main/java/com/seungjoon/algo/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import com.seungjoon.algo.user.domain.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -32,6 +33,7 @@ public class SecurityConfig{
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors(Customizer.withDefaults()) //TEST
                 .csrf(csrf -> csrf.disable())
                 .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable())
