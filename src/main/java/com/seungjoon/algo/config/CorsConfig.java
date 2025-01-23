6a253c99b7d69b23a837ccc4f81b2d5339ca7a83
Seungjoon Choi <tmdwns0845@naver.com>
2025-01-23 13:04:55 +0900

    rename

--- src/main/java/com/seungjoon/algo/config/CorsConfig.java
+++ src/main/java/com/seungjoon/algo/config/CorsConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //TEST - cors 설정
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
