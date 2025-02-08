6eed487e11bf3b2f03e6b104357a0a8605754165
Seungjoon Choi <tmdwns0845@naver.com>
2025-02-08 19:46:55 +0900

    test

--- src/test/java/com/seungjoon/algo/recruit/service/RecruitPostServiceTest.java
+++ src/test/java/com/seungjoon/algo/recruit/service/RecruitPostServiceTest.java
@@ -2,29 +2,24 @@ package com.seungjoon.algo.recruit.service;
 
 import com.seungjoon.algo.exception.BadRequestException;
 import com.seungjoon.algo.member.domain.Member;
-import com.seungjoon.algo.member.domain.MemberState;
-import com.seungjoon.algo.member.domain.Role;
 import com.seungjoon.algo.member.repository.MemberRepository;
 import com.seungjoon.algo.recruit.domain.RecruitPost;
 import com.seungjoon.algo.recruit.repository.ApplicantRepository;
 import com.seungjoon.algo.recruit.repository.RecruitPostRepository;
-import com.seungjoon.algo.study.domain.StudyRule;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.DayOfWeek;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.BDDMockito.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.mock;
 
 @ExtendWith(MockitoExtension.class)
 @Transactional
@@ -45,19 +40,17 @@ class RecruitPostServiceTest {
     @Test
     void sameAuthorAndApplicant() {
         //given
-        Member author = member(1L);
-        RecruitPost post = recruitPost(1L, author);
+        Member author = mock(Member.class);
+        given(author.getId()).willReturn(1L);
+
+        RecruitPost post = mock(RecruitPost.class);
+        given(post.getId()).willReturn(1L);
+        given(post.getMember()).willReturn(author);
+
         given(recruitPostRepository.findById(post.getId()))
                 .willReturn(Optional.of(post));
         given(memberRepository.findById(author.getId()))
                 .willReturn(Optional.of(author));
-
-        given(recruitPostRepository.findById(post.getId()))
-                .willReturn(Optional.of(post));
-
-        given(memberRepository.findById(author.getId()))
-                .willReturn(Optional.of(author));
-
         //when
 
         //then
@@ -70,9 +63,15 @@ class RecruitPostServiceTest {
     @Test
     void duplicateApplicant() {
         //given
-        Member author = member(1L);
-        RecruitPost post = recruitPost(1L, author);
-        Member applicant = member(2L);
+        Member author = mock(Member.class);
+        given(author.getId()).willReturn(1L);
+
+        RecruitPost post = mock(RecruitPost.class);
+        given(post.getId()).willReturn(1L);
+        given(post.getMember()).willReturn(author);
+
+        Member applicant = mock(Member.class);
+        given(applicant.getId()).willReturn(2L);
 
         given(recruitPostRepository.findById(post.getId()))
                 .willReturn(Optional.of(post));
@@ -93,39 +92,5 @@ class RecruitPostServiceTest {
                 .isInstanceOf(BadRequestException.class)
                 .extracting("code")
                 .isEqualTo(1012);
-    }
-
-    private Member member(Long id) {
-        Member member = Member.builder()
-                .email("test@test.com")
-                .username("test")
-                .role(Role.MEMBER)
-                .state(MemberState.ACTIVE)
-                .authType("normal")
-                .build();
-
-        ReflectionTestUtils.setField(member, "id", id);
-
-        return member;
-    }
-
-    private RecruitPost recruitPost(Long id, Member member) {
-
-        StudyRule studyRule = StudyRule.builder()
-                .submitPerWeek(10)
-                .totalWeek(5)
-                .submitDayOfWeek(DayOfWeek.FRIDAY)
-                .build();
-
-        RecruitPost post = RecruitPost.builder()
-                .title("title")
-                .content("content")
-                .member(member)
-                .studyRule(studyRule)
-                .build();
-
-        ReflectionTestUtils.setField(post, "id", id);
-
-        return post;
     }
 }
