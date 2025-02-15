bb11ac292f571cebb7266504d1d90c5246d7c70c
Seungjoon Choi <tmdwns0845@naver.com>
2025-02-15 22:56:16 +0900

    refactor

--- src/main/java/com/seungjoon/algo/utils/QuerydslUtils.java
+++ src/main/java/com/seungjoon/algo/utils/QuerydslUtils.java
@@ -3,21 +3,18 @@ package com.seungjoon.algo.utils;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.EntityPathBase;
-import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.PathBuilder;
 import org.springframework.data.domain.Pageable;
 
-//TODO
 public abstract class QuerydslUtils {
 
     public static <T> OrderSpecifier<?>[] getSort(Pageable pageable, EntityPathBase<T> qClass) {
-        return pageable.getSort().stream()
-                .map(sort -> {
-                    Order direction = Order.valueOf(sort.getDirection().name());
-                    return new OrderSpecifier(
-                            direction,
-                            Expressions.path(Object.class, qClass, sort.getProperty())
-                    );
-                }).toList()
-                .toArray(new OrderSpecifier[0]);
+
+        PathBuilder<T> pathBuilder = new PathBuilder<>(qClass.getType(), qClass.getMetadata());
+
+        return pageable.getSort().stream().map(sort -> new OrderSpecifier<>(
+                            sort.isAscending() ? Order.ASC : Order.DESC,
+                            pathBuilder.getString(sort.getProperty())
+        )).toArray(OrderSpecifier[]::new);
     }
 }
