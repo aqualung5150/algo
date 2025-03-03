72fc429984fae8e258272713342d28a70d60cb06
Seungjoon Choi <tmdwns0845@naver.com>
2025-03-03 21:47:38 +0900

    refactor: @UniqueElements

--- src/main/java/com/seungjoon/algo/study/dto/CreateStudyRequest.java
+++ src/main/java/com/seungjoon/algo/study/dto/CreateStudyRequest.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.hibernate.validator.constraints.UniqueElements;
 
 import java.util.List;
 
@@ -20,5 +21,6 @@ public class CreateStudyRequest {
 
     @NotNull
     @Size(min = 2, max = 4, message = "스터디의 팀원 수는 2 ~ 4명 사이여야 합니다.")
+    @UniqueElements
     private List<Long> memberIds;
 }
