d731636cb534f4624f7a6ec50455e6fd22c2246a
Seungjoon Choi <tmdwns0845@naver.com>
2025-04-08 16:07:00 +0900

    fix: 쿠키 삭제 도메인 설정

--- src/main/java/com/seungjoon/algo/auth/CustomLogoutSuccessHandler.java
+++ src/main/java/com/seungjoon/algo/auth/CustomLogoutSuccessHandler.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
@@ -11,12 +12,16 @@ import java.io.IOException;
 
 public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
 
+    @Value("${base-url}")
+    private String baseUrl;
+
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         // access_token 삭제
         Cookie accessToken = new Cookie("access_token", null);
         accessToken.setPath("/");
         accessToken.setMaxAge(0);
+        accessToken.setDomain(baseUrl);
         accessToken.setSecure(true);
         accessToken.setHttpOnly(true);
         response.addCookie(accessToken);
@@ -25,6 +30,7 @@ public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
         Cookie refreshToken = new Cookie("refresh_token", null);
         refreshToken.setPath("/");
         refreshToken.setMaxAge(0);
+        refreshToken.setDomain(baseUrl);
         refreshToken.setSecure(true);
         refreshToken.setHttpOnly(true);
         response.addCookie(refreshToken);
