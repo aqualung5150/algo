cb9eab4c6893e4f2e990e0a471ee82ee84af62f2
Seungjoon Choi <tmdwns0845@naver.com>
2025-04-18 10:08:42 +0900

    fix: TimeZone Asia/Seoul

--- src/main/java/com/seungjoon/algo/config/CloseStudyScheduler.java
+++ src/main/java/com/seungjoon/algo/config/CloseStudyScheduler.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.seungjoon.algo.study.domain.StudyState.COMPLETED;
